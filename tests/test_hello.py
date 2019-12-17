368f5ede68a330c005fe2e59211c80a9de0850d9
bb912 <brettbissey@gmail.com>
2019-12-17 16:38:59 -0500

    fixed tests?

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,12 +1,12 @@
 import pytest
 import sys
-sys.path.append("../pyCL")
-import one
+sys.path.append("/Users/brettbissey/pyCI/")
+from one import func
 
 #should fail
 def test_answer():
-    assert one.func() == False
+    assert func() == False
 
 #should pass
 def test_2():
-    assert one.func() == True
+    assert func() == True
