fc406d0bc266ecfa094776c7af551a1818ea6b26
bb912 <brettbissey@gmail.com>
2019-12-17 16:58:37 -0500

    merging unsure of outcome

---
+++ tests/test_hello.py
@@ -0,0 +1,13 @@
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# import ../db.py
+import one
+
+#should fail
+def test_answer():
+    assert one.func() == True
+
+#should pass
+def test_2():
+    assert one.func() == True
